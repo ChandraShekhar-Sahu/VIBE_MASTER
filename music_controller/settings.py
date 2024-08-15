175930c7c30bdb5d3f580de717038f950cdd563f
ChandraShekhar-Sahu <144163507+ChandraShekhar-Sahu@users.noreply.github.com>
2024-08-16 02:16:48 +0530

    vibe_master

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-rfxjj0bv$^ywyrm!6qsiianj8z4s(x&nrjzvxk6=g1gjpw*j!+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
